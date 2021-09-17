dbfa3552a9c81412433b1fde8ff3b249dda8dc55
Raymond Nazaryan <sudoheader@users.noreply.github.com>
2021-09-16 22:35:09 -0700

    added merge two binary trees

---
+++ 617/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+
+        if(root1 == null) {
+            return root2;
+        }
+
+        if(root2 == null) {
+            return root1;
+        }
+
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
+    }
+}
