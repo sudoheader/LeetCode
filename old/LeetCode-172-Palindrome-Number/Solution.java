0f6ac0ef93ff7bfe3a35084e26579347d7ed7784
Raymond Nazaryan <sudoheader@users.noreply.github.com>
2021-09-16 19:57:54 -0700

    added binary tree inorder traversal

--- old/LeetCode-172-Palindrome-Number/Solution.java
+++ old/LeetCode-172-Palindrome-Number/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isPalindrome(int x) {
+    public static boolean isPalindrome(int x) {
         if(x < 0 || (x % 10 == 0 && x != 0)) return false;
         int reversedNum = 0;
         while(x > reversedNum) {
